52f3ab61d60f9aa8eb336173bf68c9860e1900cf
Leandro <llimas@outlook.com>
2019-07-04 21:31:34 -0300

    Buscando descrição na catho e pegando o conteúdo delas.

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
-let keyWords = ['php', 'nodejs'];
-let text1 = 'olá,tudo bem php sakdaskdsaa nodejs nodejs';
-let text2 = 'laravel php tudo bem';
-let allTextArr = [];
-allTextArr.push(text1,text2);
-let allWordsArray = allTextArr.map(element => {
-    return element.split(/[^a-zA-Z0-9s|]/);
-}).join(',').split(',');
-let wordCounterArr = [];
-allWordsArray.forEach(wordToFind => {
-    let counter = 0;
-    allWordsArray.forEach((word,index) => {
-        if(wordToFind == word){
-            counter++;
-            delete allWordsArray[index]
+const axios = require('axios');
+const cheerio = require('cheerio');
+axios.get('https://www.catho.com.br/vagas/?q=Programador+JavaScript&estado_id%5B0%5D=25&perfil_id=1&where_search=1&how_search=2&faixa_sal_id_combinar=1&order=score&pais_id=31&page=1')
+.then(response => {
+    const $ = cheerio.load(response.data);
+    var allDescriptionsWordsArr = $('.descricaoVaga').text().split(/[^a-zA-Z0-9s|]/);
+    let wordCounterArr = [];
+    allDescriptionsWordsArr.forEach(wordToFind => {
+        if(wordToFind.length >= 4){
+            let counter = 0;
+            allDescriptionsWordsArr.forEach((word,index) => {
+                if(wordToFind == word){
+                    counter++;
+                    delete allDescriptionsWordsArr[index]
+                }
+            });
+            wordCounterArr.push({
+                qtdOcurrency: counter,
+                word: wordToFind
+            });
+            counter = 0;
         }
     });
-    wordCounterArr.push({
-        qtdOcurrency: counter,
-        word: wordToFind
-    });
-    counter = 0;
+    console.log(wordCounterArr);
 });
-console.log(wordCounterArr);
